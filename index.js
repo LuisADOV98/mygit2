30d53fe7d1cefa84a43f38545c6b259ad5d8b36f
LuisADOV98 <luisadov26@gmail.com>
2023-07-17 23:11:37 +0200

    reto5

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+
+
+function restar(x,y=0) {
+    let resultado = x-y
+    return resultado
+}
+
+function producto(x,y=0) {
+    let resultado = x*y
+    return resultado
+}
+
+function division(x,y=0) {
+    let resultado = x/y
+    return resultado
+}
+
+function elevadoAl(n, n1) {
+
+    let resultado = n ** n1
+    return resultado
+}
+
+
+
+
 // crea una carpeta en tu ordenador y abrela en vscode, luego crea un archivo index.js (javascript). al que le daras en la terminal que le corresponde "npm init",
 // aceptas hasta que salga yes y vuelves a aceptar. acto seguido escribes en la terminal "npm install".
 // En la misma terminal pones "git init". luego vas a github, creas un nuevo repositorio con readme, lo clonas y escribes en la terminal "git clone "enlace" ".
--- reto.md
+++ reto.md
@@ -3,7 +3,9 @@
 
 
 
-he creado una carpeta .gitignore y he subido las fotos, luego he hecho git pull, git add . , git commit "cambios1" y git push
+he creado una carpeta .gitignore y he subido las fotos, luego he hecho git pull, git add . , git commit -m"cambios1" y git push
+se ha creado una carpeta donde se quedan las fotos que no quieres usar
+
 
 
 
