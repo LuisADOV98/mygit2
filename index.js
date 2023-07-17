aa24c3a32c037754b179767f51c8c98171d28ebe
LuisADOV98 <luisadov26@gmail.com>
2023-07-17 23:33:07 +0200

    5

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 // aceptas hasta que salga yes y vuelves a aceptar. acto seguido escribes en la terminal "npm install".
 // En la misma terminal pones "git init". luego vas a github, creas un nuevo repositorio con readme, lo clonas y escribes en la terminal "git clone "enlace" ".
 
+
+
+
+function sumar(x,y=0) {
+    let resultado = x + y
+    return resultado
+}let resultado
