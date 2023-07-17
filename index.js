91d6ee5e8e540c4f57d44788b41b149b598245d7
LuisADOV98 <luisadov26@gmail.com>
2023-07-17 22:08:04 +0200

    Tutorial

---
+++ index.js
@@ -0,0 +1,4 @@
+// crea una carpeta en tu ordenador y abrela en vscode, luego crea un archivo index.js (javascript). al que le daras en la terminal que le corresponde "npm init",
+// aceptas hasta que salga yes y vuelves a aceptar. acto seguido escribes en la terminal "npm install".
+// En la misma terminal pones "git init". luego vas a github, creas un nuevo repositorio con readme, lo clonas y escribes en la terminal "git clone "enlace" ".
+
