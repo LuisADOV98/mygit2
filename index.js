73fa2e0802f4da29508bf5027888fdab8c6edbd4
LuisADOV98 <luisadov26@gmail.com>
2023-07-18 15:43:27 +0200

    merge rama2

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// crea una carpeta en tu ordenador y abrela en vscode, luego crea un archivo index.js (javascript). al que le daras en la terminal que le corresponde "npm init",
-// aceptas hasta que salga yes y vuelves a aceptar. acto seguido escribes en la terminal "npm install".
-// En la misma terminal pones "git init". luego vas a github, creas un nuevo repositorio con readme, lo clonas y escribes en la terminal "git clone "enlace" ".
 
 
-
-
-function sumar(x,y=0) {
-    let resultado = x + y
+function sumar(x,y,z) {
+    let resultado = x + y + z
     return resultado
 }let resultado
 
@@ -31,5 +26,10 @@ function elevadoAl(n, n1) {
     let resultado = n ** n1
     return resultado
 }
+
+
+// crea una carpeta en tu ordenador y abrela en vscode, luego crea un archivo index.js (javascript). al que le daras en la terminal que le corresponde "npm init",
+// aceptas hasta que salga yes y vuelves a aceptar. acto seguido escribes en la terminal "npm install".
+// En la misma terminal pones "git init". luego vas a github, creas un nuevo repositorio con readme, lo clonas y escribes en la terminal "git clone "enlace" ".
 
 
