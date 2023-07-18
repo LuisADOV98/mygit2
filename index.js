20aa7f357b5ce5e5a3bbd59f645bf1d2cc120f1d
LuisADOV98 <luisadov26@gmail.com>
2023-07-18 15:50:15 +0200

    de nuevo

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
 <<<<<<< HEAD
 
 
-function restar(x,y=0) {
-    let resultado = x-y
-    return resultado
-}
-
-function producto(x,y=0) {
-    let resultado = x*y
-    return resultado
-}
-
-function division(x,y=0) {
-    let resultado = x/y
-    return resultado
-}
-
-function elevadoAl(n, n1) {
-
-    let resultado = n ** n1
-    return resultado
-}
 
 
-
-
-// crea una carpeta en tu ordenador y abrela en vscode, luego crea un archivo index.js (javascript). al que le daras en la terminal que le corresponde "npm init",
-// aceptas hasta que salga yes y vuelves a aceptar. acto seguido escribes en la terminal "npm install".
-// En la misma terminal pones "git init". luego vas a github, creas un nuevo repositorio con readme, lo clonas y escribes en la terminal "git clone "enlace" ".
-=======
->>>>>>> rama2
-
-
-function sumar(x,y,z) {
-    let resultado = x + y + z
-    return resultado
-}let resultado
 
 
 function restar(x,y=0) {
@@ -58,10 +25,4 @@ function elevadoAl(n, n1) {
     let resultado = n ** n1
     return resultado
 }
-
-
-// crea una carpeta en tu ordenador y abrela en vscode, luego crea un archivo index.js (javascript). al que le daras en la terminal que le corresponde "npm init",
-// aceptas hasta que salga yes y vuelves a aceptar. acto seguido escribes en la terminal "npm install".
-// En la misma terminal pones "git init". luego vas a github, creas un nuevo repositorio con readme, lo clonas y escribes en la terminal "git clone "enlace" ".
-
 
